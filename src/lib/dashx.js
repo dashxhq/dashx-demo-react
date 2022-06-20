0dd3a0269af3d649956b58b5061e8adba70682b9
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-06-20 11:44:27 +0530

    added underscore in example env

--- src/lib/dashx.js
+++ src/lib/dashx.js
@@ -1,7 +1,7 @@
 import DashX from '@dashx/web'
 
 const dashx = DashX({
-  baseUri: process.env.REACT_APP_DASHX_BASEURI,
+  baseUri: process.env.REACT_APP_DASHX_BASE_URI,
   publicKey: process.env.REACT_APP_DASHX_PUBLIC_KEY,
   targetEnvironment: process.env.REACT_APP_DASHX_TARGET_ENVIRONMENT
 })
