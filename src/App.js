a487b158eb9c308caf14420de8fbe37ff4ba099f
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-06-21 22:12:13 +0530

    modified routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Layout from './components/Layout'
 import DashboardPage from './pages/DashboardPage'
 import { useAuth } from './components/contexts/CurrentUserProvider'
 import ForgotPassword from './pages/ForgotPassword'
+import DashboardHome from './components/Dashboard/DashboardHome';
+import UpdateProfileForm from './components/forms/UpdateProfileForm';
+import DashboardLayout from './components/Dashboard/DashboardLayout';
+import Bookmarks from './components/Dashboard/Bookmarks';
+import Billing from './components/Dashboard/Billing';
+import Settings from './components/Dashboard/Settings';
 
 const App = () => {
   const { user, setUser } = useAuth()
@@ -20,11 +26,56 @@ const App = () => {
           <Route exact path="forgot-password" element={<ForgotPassword />} />
           <Route exact path="register" element={<RegisterForm />} />
           <Route
-            exact
             path="dashboard"
             element={
               <RequireAuth user={user} setUser={setUser}>
                 <DashboardPage />
+              </RequireAuth>
+            }
+          >
+            <Route index element={<DashboardHome />} />
+          </Route>
+          <Route
+            exact
+            path="update-profile"
+            element={
+              <RequireAuth user={user} setUser={setUser}>
+                <DashboardLayout>
+                  <UpdateProfileForm />
+                </DashboardLayout>
+              </RequireAuth>
+            }
+          />
+          <Route
+            exact
+            path="bookmarks"
+            element={
+              <RequireAuth user={user} setUser={setUser}>
+                <DashboardLayout>
+                  <Bookmarks />
+                </DashboardLayout>
+              </RequireAuth>
+            }
+          />
+          <Route
+            exact
+            path="billing"
+            element={
+              <RequireAuth user={user} setUser={setUser}>
+                <DashboardLayout>
+                  <Billing />
+                </DashboardLayout>
+              </RequireAuth>
+            }
+          />
+          <Route
+            exact
+            path="settings"
+            element={
+              <RequireAuth user={user} setUser={setUser}>
+                <DashboardLayout>
+                  <Settings />
+                </DashboardLayout>
               </RequireAuth>
             }
           />
