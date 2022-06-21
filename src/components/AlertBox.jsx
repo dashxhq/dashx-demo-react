176c6f92c2bd7f8406ad33d9c8a97c7414751a26
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-06-21 13:35:33 +0530

    modified alertbox and input styles

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -5,7 +5,7 @@ const AlertBox = ({ alertMessage }) => (
     className="mt-5 p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
     role="alert"
   >
-    <span className="font-medium">{alertMessage}</span>
+    <span className="font-medium ml-6">{alertMessage}</span>
   </div>
 )
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ErrorMessage, useField } from 'formik'
 
 const classes = {
-  input: 'appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+  input: 'appearance-none block w-full py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
 }
 
 const Input = (props) => {
