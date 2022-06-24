700d5edb0bd8fca40f000c4fabd5e3bf1f86aa5b
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-06-24 19:02:14 +0530

    Added index.html file to the config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 const defaultTheme = require('tailwindcss/defaultTheme')
 const tailwindForms = require('@tailwindcss/forms')
 
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}'
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
       fontFamily: {
-        sans: [ 'Inter var', ...defaultTheme.fontFamily.sans ]
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
       }
     },
     screens: {
@@ -20,5 +17,5 @@ module.exports = {
       '2xl': '1536px'
     }
   },
-  plugins: [ tailwindForms ]
+  plugins: [tailwindForms]
 }
