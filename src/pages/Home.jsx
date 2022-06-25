21d897d3da167b9ef51b75928f952eb2be726422
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-06-25 23:36:10 +0530

    Changed Dashboard heading to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => (
   <div>
-    <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+    <h1 className="text-2xl font-semibold text-gray-900">Home</h1>
   </div>
 )
 
