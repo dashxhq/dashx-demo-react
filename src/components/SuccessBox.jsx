3a0acc9f34f4d3447cc04897bdc81ebd3a97827f
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-06-26 14:34:33 +0530

    Added classes prop for accepting custom tailwind classes

--- src/components/SuccessBox.jsx
+++ src/components/SuccessBox.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const SuccessBox = ({ successMessage }) => (
+const SuccessBox = ({ successMessage, classes }) => (
   <div
-    className="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800 my-2"
+    className={
+      `p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800 my-2
+      ${classes}`
+    }
     role="alert"
   >
     <span className="font-medium">{successMessage}</span>
