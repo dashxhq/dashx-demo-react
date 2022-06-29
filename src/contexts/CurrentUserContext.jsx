9229df8f66d21b2945cd9a8859d447fc8f259eee
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-06-29 15:58:13 +0530

    Removed String wrapper for user id in dashx.setIdentity() and added  API call

--- src/contexts/CurrentUserContext.jsx
+++ src/contexts/CurrentUserContext.jsx
@@ -1,7 +1,8 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import jwtDecode from 'jwt-decode'
 
 import dashx from '../lib/dashx'
+import api from '../lib/api'
 
 const jwtToken = localStorage.getItem('jwt-token')
 const decodedUser = jwtToken ? jwtDecode(jwtToken).user : null
@@ -11,12 +12,27 @@ const CurrentUserContext = createContext(null)
 const CurrentUserProvider = ({ children }) => {
   const [user, setUser] = useState(decodedUser)
 
+  useEffect(() => {
+    const getProfile = async () => {
+      const { data: { user } = {} } = await api.get('/profile', {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`
+        }
+      })
+      setUser(user)
+    }
+
+    if (jwtToken) {
+      getProfile()
+    }
+  }, [])
+
   const login = (jwtToken) => {
     localStorage.setItem('jwt-token', jwtToken)
     const decodedToken = jwtDecode(jwtToken)
     const dashxToken = decodedToken.dashx_token
     const decodedUser = decodedToken.user
-    dashx.setIdentity(String(decodedUser.id), dashxToken)
+    dashx.setIdentity(decodedUser.id, dashxToken)
     setUser(decodedUser)
   }
 
