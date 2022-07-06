69a02002598d5a6b5cf2cd92b496076897bad011
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-07-06 17:22:55 +0530

    Fetch and add post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,91 +3,26 @@ import React, { useEffect, useState } from 'react'
 import Modal from '../components/Modal'
 import Button from '../components/Button'
 
-const postLists = [
-  {
-    id: 1,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,',
-    user_id: 30,
-    created_at: '2022-07-03T18:30:00.000Z',
-    updated_at: '2022-07-03T18:30:00.000Z',
-    image: null,
-    video: null,
-    first_name: 'Rahul',
-    last_name: 'SR',
-    email: 'abc@gmail.com'
-  },
-  {
-    id: 2,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,',
-    user_id: 30,
-    created_at: '2022-07-03T18:30:00.000Z',
-    updated_at: '2022-07-03T18:30:00.000Z',
-    image: null,
-    video: null,
-    first_name: 'Rahul',
-    last_name: 'SR',
-    email: 'abc@gmail.com'
-  },
-  {
-    id: 3,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,',
-    user_id: 30,
-    created_at: '2022-07-03T18:30:00.000Z',
-    updated_at: '2022-07-03T18:30:00.000Z',
-    image: null,
-    video: null,
-    first_name: 'Rahul',
-    last_name: 'SR',
-    email: 'abc@gmail.com'
-  },
-  {
-    id: 4,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,',
-    user_id: 30,
-    created_at: '2022-07-03T18:30:00.000Z',
-    updated_at: '2022-07-03T18:30:00.000Z',
-    image: null,
-    video: null,
-    first_name: 'Rahul',
-    last_name: 'SR',
-    email: 'abc@gmail.com'
-  },
-  {
-    id: 5,
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,',
-    user_id: 30,
-    created_at: '2022-07-03T18:30:00.000Z',
-    updated_at: '2022-07-03T18:30:00.000Z',
-    image: null,
-    video: null,
-    first_name: 'Rahul',
-    last_name: 'SR',
-    email: 'abc@gmail.com'
-  }
-]
+import api from '../lib/api'
 
 const Post = ({ post }) => {
+  const { id, created_at, text, user } = post || {}
+  const { first_name, last_name } = user
+
   return (
-    <div className="mb-4">
-      <div className="relative flex gap-4">
-        <img
-          className="h-10 w-10 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          alt=""
-        />
-        <span className="absolute -bottom-0.5 -right-1 bg-white rounded-tl px-0.5 py-px"></span>
-        <div className="min-w-0 flex-1">
-          <div>
-            <div className="text-sm font-medium">
-              {post.first_name} {post.last_name}
-            </div>
-            <p className="mt-0.5 text-sm text-gray-500">
-              Posted {new Date(post.created_at).getDay()}d ago
-            </p>
-          </div>
-          <div className="mt-2 text-sm text-gray-700">
-            <p>{post.text}</p>
-          </div>
+    <div className="relative flex gap-4 mb-4" key={id}>
+      <img
+        className="h-10 w-10 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white"
+        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+        alt=""
+      />
+      <div className="min-w-0 flex-1">
+        <p className="text-sm font-medium">
+          {first_name}&nbsp;{last_name}
+        </p>
+        <p className="mt-0.5 text-sm text-gray-500">Posted {new Date(created_at).getDay()}d ago</p>
+        <div className="mt-2 text-sm text-gray-700">
+          <p>{text}</p>
         </div>
       </div>
     </div>
@@ -95,12 +30,41 @@ const Post = ({ post }) => {
 }
 
 const Home = () => {
-  const [posts, setPosts] = useState(postLists)
+  const [postsList, setPostsList] = useState([])
   const [isModalOpen, setIsModalOpen] = useState(false)
 
+  async function fetchPosts() {
+    const params = {
+      params: {
+        limit: 5,
+        offset: 5
+      }
+    }
+    try {
+      const { data: { posts } = {} } = await api.get('/posts')
+      setPostsList(posts)
+    } catch (error) {
+      console.log(error, 'error - fetchPosts')
+    }
+  }
+
   useEffect(() => {
-    console.log('Came herer for fetching')
-  }, [isModalOpen])
+    fetchPosts()
+  }, [])
+
+  const handleSubmit = async (values, resetForm) => {
+    try {
+      setIsModalOpen(false)
+      const { status } = await api.post('/posts', values)
+      if (status === 200) {
+        setIsModalOpen(false)
+        await fetchPosts()
+        resetForm()
+      }
+    } catch (error) {
+      console.log(error, 'error')
+    }
+  }
 
   return (
     <div className="m-auto">
@@ -110,10 +74,14 @@ const Home = () => {
           <Button label="Add Post" loading={false} onClick={() => setIsModalOpen(true)} />
         </div>
       </div>
-      {posts.map((post) => (
-        <Post post={post} key={post.id} />
-      ))}
-      <Modal open={isModalOpen} setOpen={setIsModalOpen} />
+      <div>
+        {postsList.length > 0 ? (
+          postsList.map((post) => <Post post={post} key={post.id} />)
+        ) : (
+          <h1 className="text-lg font-medium text-gray-900">No Posts</h1>
+        )}
+      </div>
+      <Modal open={isModalOpen} setOpen={setIsModalOpen} handleSubmit={handleSubmit} />
     </div>
   )
 }
