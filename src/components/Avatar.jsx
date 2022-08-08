dfcaf6ad8cdbdce25ec9c51f3f785da1a277abf7
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-08-08 15:16:09 +0530

    Add avatar component

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { useCurrentUserContext } from '../contexts/CurrentUserContext'
+
+const Avatar = () => {
+  const { user } = useCurrentUserContext()
+  const { avatar, first_name, last_name, email } = user || {}
+  const initials = `${first_name.charAt(0)}${last_name.charAt(0)}`
+  const emailInitials = `${email.charAt(0)}`
+  const avatarUrl =
+    avatar?.url || `https://ui-avatars.com/api/?name=${initials || emailInitials}&size=96`
+
+  return (
+    <div className="flex items-center justify-center">
+      <img src={avatarUrl} className="rounded-full w-10 h-10" alt="Avatar" />
+    </div>
+  )
+}
+
+export default Avatar
