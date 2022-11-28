62964cd63e5ec00149319395288601dd354450c0
Mohansingh Omprakash <mohan.singh1385664@gmail.com>
2022-11-28 16:38:18 +0530

    Add dx.track() and dx.identify() (#15)

--- src/components/FormHeader.jsx
+++ src/components/FormHeader.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import DashXLogo from '../assets/dashx_logo_black.png'
 
 const FormHeader = ({ children }) => (
   <div className="sm:mx-auto sm:w-full sm:max-w-md">
     <div className="flex justify-center items-center">
-      <img src={DashXLogo} className="h-12 w-12" alt="DashX Logo" />
+      <Link to="/">
+        <img src={DashXLogo} className="h-12 w-12" alt="DashX Logo" />
+      </Link>
     </div>
     <h2 className="mt-6 mb-6 text-center text-3xl font-extrabold text-gray-900">{children}</h2>
   </div>
